c2d164f5416f828d9d4ae5f9ad5b8655b242b0ce
ikhsan5117 <ikhsanmuhammad37956@gemail.com>
2025-10-30 20:42:40 +0700

    update

--- lib/latihan/DasarState.dart
+++ lib/latihan/DasarState.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class Dasarstate extends StatelessWidget {
+class Dasarstate extends StatefulWidget {
   const Dasarstate({super.key});
 
   @override
+  State<Dasarstate> createState() => _DasarstateState();
+}
+
+class _DasarstateState extends State<Dasarstate> {
+  bool isDark = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ambilData();
+  }
+  
+  // function untuk menyimpan data ke shared preferences
+  void simpanData()async {
+    final p = await SharedPreferences.getInstance();
+    await p.setBool("temaGelap", isDark);
+  }
+
+  void ambilData()async{
+    final p = await SharedPreferences.getInstance();
+    setState(() {
+      isDark = p.getBool("temaGelap") ?? false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "Latihan Shared Preference",
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Switch Theme"),
+        ),
+        body: Center(
+          child: SwitchListTile(
+            title: const Text("tombol tema"),
+            value: isDark,
+            onChanged: (bool value) {
+              setState(() {
+                isDark = value;
+              });
+              simpanData();
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
