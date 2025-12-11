bfd3ddf079972b34505ff54bea7c566e1ef149f6
ikhsan5117 <ikhsanmuhammad37956@gemail.com>
2025-12-11 21:36:58 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // import file lain
-import 'package:application_1/latihan/DasarState.dart';
+import 'package:application_1/latihan/text_box.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Dasarstate(),
+      home: TextBoxPage(),
     );
   }
 }
